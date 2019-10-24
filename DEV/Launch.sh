4bf7cfc9bada5d71e1806907bb16e5b02e555e2c
durandau <55870914+durandau@users.noreply.github.com>
2019-10-24 16:31:25 +0200

    Update Launch.sh

--- DEV/Launch.sh
+++ DEV/Launch.sh
@@ -3,15 +3,12 @@
 export PATH =$PATH:/~riscV64/bin
 echo 'export done'
 echo '================================'
-./buildNexys4ddr.py
-echo 'buildNexys4ddr done'
-echo '================================'
-./Load.py
+./load.py
 echo 'Load done'
 echo '================================'
 cd Firmware
-make
+make all clean
 echo 'make done'
 echo '================================'
 cd..
-python3.7 litex/litex/tools/litex_term.py /dev/ttyUSB1 --kernel Firmware/Firmware.bin
+python3.7 litex/litex/tools/litex_term.py /dev/ttyUSB1 --kernel Firmware/firmware.bin
