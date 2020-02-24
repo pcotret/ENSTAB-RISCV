e1c7e8ecb72d97d241db103eedb9fbf4d8cd907b
durandau <55870914+durandau@users.noreply.github.com>
2020-02-24 15:42:23 +0100

    Update snake.c

--- DEV/firmware/snake.c
+++ DEV/firmware/snake.c
@@ -3,98 +3,163 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include <generated/csr.h>
+
 #include "spijoystick.h"
 #include "PmodJSTK2.h"
 #include "display.h"
 #include "delay.h"
+#include "color.h"
 
 void snake(void) {
+
+	char flag_end = 0;
+	unsigned char flag_over=0;
+	
 	unsigned char fsButtons=0;
 	unsigned char stdPktValues [5];
 	unsigned char rtrnValues [2];
-	signed char x,y;
+	signed char joy_x,joy_y;
 	
-	char mvt=0, lastmvt=4;	//1-> up; 2->down; 3->left; 4->right
-							//1->left; 2->right; 3->up; 4->down
-	char seg = 0;
-	char motif[8] = {0,0,0,0,0,0,0,0};
+	char txt[8]; // for 7seg
 	
-	int i, digit = 0;
+	unsigned short thikness = 8;
+	unsigned int x_rez = 640;
+	unsigned int y_rez = 480;
 	
-	motif[digit] = ~(1<<seg);
-
-	while((fsButtons&bitJstk)==0) {
-		//setCommand(cmdSetInversionY,0,0,0,0,stdPktValues);
-		//lecture joystick
-		getCommand(cmdGetPosition,2,stdPktValues,rtrnValues);
-		fsButtons = stdPktValues[4];
-		x = rtrnValues[0]-128;
-		y = rtrnValues[1]-128;
+	unsigned short sn_x[255], sn_y[255];
+	unsigned char taille;
 	
-		//choix mvt
-		if(85 <= (abs(x)|abs(y))) {
-			//if(abs(x)>=abs(y)) mvt = ((x>0) ? ((mvt==3) ? 3 : 4) : ((mvt==4) ? 4 : 3));
-			//else mvt = ((y>0) ? ((mvt==2) ? 2 : 1) : ((mvt==1) ? 1 : 2));
-			if(abs(x)>=abs(y)) mvt = ((x>0) ? ((mvt==2) ? 2 : 1) : ((mvt==1) ? 1 : 2));
-			else mvt = ((y>0) ? ((mvt==4) ? 4 : 3) : ((mvt==3) ? 3 : 4));
-		
-			//maj motif
-			switch(mvt) {
-				case 1:
-					switch (seg) {
-						case 2:	seg = 1;
-							break;
-						case 3:	seg = ((lastmvt == 3) ? 4 : ((lastmvt == 4) ? 2 : seg));
-							break;
-						case 4: seg = 5;
-							break;
-						case 6:	seg = ((lastmvt == 3) ? 5 : ((lastmvt == 4) ? 1 : seg));
-							break;
-						default: mvt=lastmvt;
-					}
-					break;
-				case 2:
-					switch (seg) {
-						case 1:	seg = 2;
-							break;
-						case 0:	seg = ((lastmvt == 3) ? 5 : ((lastmvt == 4) ? 1 : seg));
-							break;
-						case 5: seg = 4;
-							break;
-						case 6:	seg = ((lastmvt == 3) ? 4 : ((lastmvt == 4) ? 2 : seg));
-							break;
-						default: mvt=lastmvt;
-					}
-					break;
-				case 3: if ((seg != 1) && (seg != 2)) digit = ((digit == 0) ? 7 : digit-1);
-				case 4:
-					if ((mvt==4) && (seg != 4) && (seg !=5)) digit = ((digit == 7) ? 0 : digit+1);
-					switch (seg) {
-						case 1: 
-						case 5:	seg = ((lastmvt == 2) ? 6 : ((lastmvt == 1) ? 0 : seg));
-							break;
-						case 2:
-						case 4:	seg = ((lastmvt == 2) ? 3 : ((lastmvt == 1) ? 6 : seg));
-							break;
-					}
-					break;
+	unsigned short ap_x, ap_y;
+	
+	char mvt;	//1->left; 2->right; 3->up; 4->down
+	
+	int x,y;
+	
+	while(flag_end == 0) {
+		// affichage fond
+		for(y=0; y<y_rez; y++) {
+			for(x=0; x<x_rez; x++) {
+				vga_cntrl_mem_we_write(0);
+				vga_cntrl_mem_adr_write(y*x_rez+x);
+				vga_cntrl_mem_data_w_write(((y<(thikness/2)) || (y>=y_rez-thikness/2) || (x<(thikness/2)) || (x>=x_rez-thikness/2)) ? color_white : 0);
+				vga_cntrl_mem_we_write(1);
 			}
 		}
 		
-		for(i=0; i<8; i++) {
-			motif[i] = ((i==digit) ? (1<<seg) : 0);
+		// maj 7segment
+		sprintf(txt,"%c%c%c%c%c%c%c%c",display_G, display_A, display_M, display_E, 0, display_O, display_N, 0);
+		display(txt);
+
+		// init snake
+		sn_x[0] = x_rez/2;
+		sn_y[0] = y_rez/2;
+		taille = 1;
+	
+		// init pomme
+		ap_x = x_rez/4*3;
+		ap_y = y_rez/2;
+		
+		// init mvt
+		mvt = 0; 
+		
+		// boucle de jeu
+		while(flag_over==0) {
+			//lecture joystick
+			getCommand(cmdGetPosition,2,stdPktValues,rtrnValues);
+			fsButtons = stdPktValues[4];
+			joy_x = rtrnValues[0]-128;
+			joy_y = rtrnValues[1]-128;
+	
+			//choix mvt
+			if(85 <= (abs(joy_x)|abs(joy_y))) {
+				if(abs(joy_x)>=abs(joy_y)) mvt = ((joy_x>0) ? ((mvt==4) ? 4 : 3) : ((mvt==3) ? 3 : 4));
+				else mvt = ((joy_y>0) ? ((mvt==1) ? 1 : 2) : ((mvt==2) ? 2 : 1));
+			}
+		
+			//effacement motif
+			for(y = sn_y[taille-1]-thikness/2; y<sn_y[taille-1]+thikness/2; y++) {
+				for(x=sn_x[taille-1]-thikness/2; x<sn_x[taille-1]+thikness/2; x++){
+					vga_cntrl_mem_we_write(0);
+					vga_cntrl_mem_adr_write(y*x_rez+x);
+					vga_cntrl_mem_data_w_write(0);
+					vga_cntrl_mem_we_write(1);
+				}
+			}
+		
+			//decallage serpent
+			for(x=taille; x>0; x--){
+				sn_x[x] = sn_x[x-1];
+				sn_y[x] = sn_y[x-1];
+			}
+		
+			//maj tete
+			switch(mvt) {
+				case 1: // left //
+					sn_x[0] -= thikness;
+					break;
+				case 2: // right //
+					sn_x[0] += thikness;
+					break;
+				case 3: // up //
+					sn_y[0] -= thikness;
+					break;
+				case 4: // down //
+					sn_y[0] += thikness;
+					break;
+			}
+		
+			//affichage serpent
+			for(y = sn_y[0]-thikness/2; y<sn_y[0]+thikness/2; y++) {
+				for(x=sn_x[0]-thikness/2; x<sn_x[0]+thikness/2; x++){
+					vga_cntrl_mem_we_write(0);
+					vga_cntrl_mem_adr_write(y*x_rez+x);
+					if(vga_cntrl_mem_data_r_read()==color_green || vga_cntrl_mem_data_r_read()==color_white){
+						flag_over = 1;
+					}
+					else vga_cntrl_mem_data_w_write(color_green);
+					vga_cntrl_mem_we_write(1);
+				}
+			}
+		
+			//Test Pomme
+			if((sn_x[0]==ap_x) && (sn_y[0] == ap_y))
+			{
+				//augmenter taille serpent
+				taille ++;
+			
+				//nouvelle pomme
+				ap_x = (rand()%(x_rez/thikness-4)+2)*thikness;
+				ap_y = (rand()%(y_rez/thikness-4)+2)*thikness; 
+			}
+		
+			//affichage pomme
+			for(y = ap_y-thikness/2; y<ap_y+thikness/2; y++) {
+				for(x=ap_x-thikness/2; x<ap_x+thikness/2; x++){
+					vga_cntrl_mem_we_write(0);
+					vga_cntrl_mem_adr_write(y*x_rez+x);
+					vga_cntrl_mem_data_w_write((y == ap_y-thikness/2) ? ((x < ap_x) ? color_black : color_bright_green) : color_red);
+					vga_cntrl_mem_we_write(1);
+				}
+			}
+		
+			//delai de boucle
+			delay_ms(100);
 		}
+	
+		// maj 7segment
+		sprintf(txt,"%c%c%c%c%c%c%c%c",display_G, display_A, display_M, display_E, display_O, display_V, display_E, display_R);
+		display(txt);
+	
+		// retour du score
+		printf("Final score: %d\nNew game ? (press joystick to continue or trigger to stop)\n", taille-1);
 		
-		//affichage motif
-		display(motif);
-		
-		delay_ms(200);
-		
-		lastmvt = mvt;
+		// test play again
+		while((fsButtons&(bitJstk+bitTrigger)) == 0){
+			getCommand(cmdGetPosition,2,stdPktValues,rtrnValues);
+			fsButtons = stdPktValues[4];
+		}
+		if(fsButtons&bitTrigger) flag_end = 1;
+		if(fsButtons&bitJstk) flag_over = 0;
 	}
-	
-	sprintf(motif,"%c%c%c%c%c%c%c%c",display_G, display_A, display_M, display_E, display_O, display_V, display_E, display_R);
-	
-	//affichage motif
-	display(motif);
 }
