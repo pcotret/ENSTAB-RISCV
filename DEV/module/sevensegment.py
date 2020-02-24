4ac0287fff58a32b38c52a10c5662ccdb126e103
durandau <55870914+durandau@users.noreply.github.com>
2020-02-24 10:34:37 +0100

    Update sevensegment.py

--- DEV/module/sevensegment.py
+++ DEV/module/sevensegment.py
@@ -7,49 +7,22 @@ from litex.soc.interconnect.csr import *
 # Seven Segment ---------------------------------------------------------------------------
 
 class SevenSegment(Module, AutoCSR):
-    """VGA display controller
-    
-    Provides a video controller circuit to drive the sync and color signals with the correct timing
-    in order to produce a working display system.
-    """
     def __init__(self, segment_out, digit_out):
         
         nb_dig = len(digit_out)
-        refresh_period = 16*10**(-3)
-        digit_period = refresh_period/nb_dig
+        dgt_count = Signal(max = nb_dig)
+	
+	refresh_period = 16*10**(-3)  # refresh of 16ms (from datasheet)
+        digit_period = refresh_period/nb_dig # period for 1 digit
         
         dgt_tick = Signal()
-        dgt_tick_count = Signal(max = int(digit_period*100e6-1))
+        dgt_tick_count = Signal(max = int(digit_period*100e6))
         
-        dgt_count = Signal(3)
-        self.displays = Array(Signal(8) for i in range(nb_dig))
+        self.displays = Array(Signal(len(segment_out)) for i in range(nb_dig))
         
-        # # #
-        
-        self.sync += [
-        	# digit tick counter
-            If(dgt_tick_count >= int(digit_period*100e6-1),
-                dgt_tick_count.eq(0),
-                dgt_tick.eq(1)
-            ).Else(
-            	dgt_tick_count.eq(dgt_tick_count + 1),
-            	dgt_tick.eq(0)
-            ),
-            # Digit counter
-	        If(dgt_tick,
-			    If(dgt_count >= nb_dig-1,
-			    	dgt_count.eq(0)
-			    ).Else(dgt_count.eq(dgt_count + 1)
-			    )
-	        )
-        ]
-        
-        self.comb += [
-        	digit_out.eq(~(1<<dgt_count)),
-        	segment_out.eq(~self.displays[dgt_count])
-        ]
-        
-        self._dig_0 = CSRStorage(8)
+	# CSR -----------------------------------------------------------------------------
+	
+	self._dig_0 = CSRStorage(8)
         self._dig_1 = CSRStorage(8)
         self._dig_2 = CSRStorage(8)
         self._dig_3 = CSRStorage(8)
@@ -68,4 +41,31 @@ class SevenSegment(Module, AutoCSR):
 		    self.displays[6].eq(self._dig_6.storage),
 		    self.displays[7].eq(self._dig_7.storage)
         ]
+	
+        # # #
         
+        self.sync += [
+            # Digit tick counter
+            If(dgt_tick_count >= int(digit_period*100e6-1),
+                dgt_tick_count.eq(0),
+                dgt_tick.eq(1)
+            ).Else(
+            	dgt_tick_count.eq(dgt_tick_count + 1),
+            	dgt_tick.eq(0)
+            ),
+            # Digit counter
+	    If(dgt_tick,
+	        If(dgt_count >= nb_dig-1,
+		    dgt_count.eq(0)
+		).Else(
+		    dgt_count.eq(dgt_count + 1)
+		)
+	    )
+        ]
+        
+        self.comb += [
+	    # digit_out
+            digit_out.eq(~(1<<dgt_count)),
+            # segment_out
+	    segment_out.eq(~self.displays[dgt_count])
+        ]
