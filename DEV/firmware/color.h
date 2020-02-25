4992441d1e038df6c92d264642958a3326402153
durandau <55870914+durandau@users.noreply.github.com>
2020-02-25 17:45:49 +0100

    Add files via upload

--- DEV/firmware/color.h
+++ DEV/firmware/color.h
@@ -1,18 +1,18 @@
 
-#define color_black				0x000000
-#define color_blue				0x0000AA
-#define color_green				0x00AA00
-#define color_cyan				0x00AAAA
-#define color_red				0xAA0000
-#define color_magenta			0xAA00AA
-#define color_brown				0xAA5500
-#define color_gray				0xAAAAAA
-#define color_dark_gray			0x555555
-#define color_bright_blue		0x5555FF
-#define color_bright_green		0x55FF55
-#define color_bright_cyan		0x55FFFF
-#define color_bright_red		0xFF5555
-#define color_bright_magenta	0xFF55FF
-#define color_yellow			0xFFFF55
-#define color_white				0xFFFFFF
+#define color_black				0x000
+#define color_blue				0x00A
+#define color_green				0x0A0
+#define color_cyan				0x0AA
+#define color_red				0xA00
+#define color_magenta			0xA0A
+#define color_brown				0xA50
+#define color_gray				0xAAA
+#define color_dark_gray			0x555
+#define color_bright_blue		0x55F
+#define color_bright_green		0x5F5
+#define color_bright_cyan		0x5FF
+#define color_bright_red		0xF55
+#define color_bright_magenta	0xF5F
+#define color_yellow			0xFF5
+#define color_white				0xFFF
 
